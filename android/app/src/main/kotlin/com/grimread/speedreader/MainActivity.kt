c422c6a7a57fbc7d91e8cce9f69d9dfb1316969b
afortunado69x-art <afortunado69x@gmail.com>
2026-03-13 18:37:33 +0100

    Register Syncfusion license in MainActivity

--- android/app/src/main/kotlin/com/grimread/speedreader/MainActivity.kt
+++ android/app/src/main/kotlin/com/grimread/speedreader/MainActivity.kt
@@ -3,3 +3,4 @@ package com.grimread.speedreader
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
+SyncfusionLicense.registerLicense('Ngo9BigBOggjHTQxAR8/V1JGaF1cXmhKYVF2WmFZfVhgdV9HYVZTQWY/P1ZhSXxVdkZiWn5dcXVXQ2dVVkZ9XEA=');
